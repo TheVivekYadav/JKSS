2265f1c141d89b2f9cc86a7b501711d33e530eeb
vivek yadav <vivekyadavclasher@gmail.com>
2023-01-28 00:31:00 +0530

    added loading screen and updated images of gallery

---
+++ js/loading_screen.js
@@ -0,0 +1,10 @@
+window.onload = function(){
+    // var logo = document.querySelector('.loading-image');
+    // console.log("loading")
+    // logo.classList.add('loading');
+    // setTimeout(function(){
+    //     logo.classList.remove('loading');
+    //     console.log("stoped loading")
+    // },5000)
+    document.getElementById("loading-screen").style.display = "none";
+}
